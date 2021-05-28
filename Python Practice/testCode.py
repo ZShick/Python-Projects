ad3fafbc66c615959980a0da09a6090628d91a07
ZShick <zshick@pdx.edu>
2021-05-28 15:37:35 -0700

    Python practice

--- Python Practice/testCode.py
+++ Python Practice/testCode.py
@@ -1,16 +1,33 @@
 
 
-mySentence = 'I love the color'
+mySentence = 'loves the color'
 
 color_list = ['red','blue','green','pink','teal','black']
 
-def color_function():
+def color_function(name):
+    lst = []
     for i in color_list:
-        print("{} {}".format(mySentence,i))
+        msg = "{0} {1} {2}".format(name,mySentence,i)
+        lst.append(msg)
+    return lst
 
 
 
 
 
 
-color_function()
+def get_name():
+    go = True
+    while go:
+        name = input('What is your name? ')
+        if name == '':
+            print("You need to provide your name!")
+        elif name == 'Sally':
+            print('Sally, you may not use this software.')
+        else:
+            go = False
+    lst = color_function(name)
+    for i in lst:
+        print(i)
+
+get_name()
