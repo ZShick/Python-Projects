7d9205fe9090ed51a06ab427656d66daccd8b6f7
ZShick <zdshick@gmail.com>
2021-08-25 18:24:35 -0700

    Update/Clean-Up

--- DjangoUniversity/Classes/classApp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import djangoClasses
-#this is importing the class I created from the .models page and letting admins create new instances of the class
-
-admin.site.register(djangoClasses)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Python Projects
- This is where I will store my initial practice and projects using Python.
